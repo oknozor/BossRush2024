90270aa9e7748d258159951718dd3efc923b47fb
Paul Delafosse <paul.delafosse@protonmail.com>
2024-01-08 18:27:46 +0100

    feat: add cardbord particle effect

--- cardboard/CardSprite.gd
+++ cardboard/CardSprite.gd
@@ -1,26 +1,53 @@
 extends Sprite2D
+class_name Card
 
-enum ScaleDirection { UP, DOWN }
+enum CardType { Attack, Defense, Ability }
+
+@export var card_type: CardType
+@onready var particles: CPUParticles2D = $CPUParticles2D
+
 const MIN_SCALE = 1.0
-const MAX_SCALE = 1.2
-
-var scale_direction = ScaleDirection.DOWN
+const MAX_SCALE = 1.4
 
 var current_scale = MIN_SCALE
+var selected = false
+var hovered = false
 
-func _process(delta):
-	match scale_direction:
-		ScaleDirection.DOWN:
-			if current_scale > MIN_SCALE:
-				current_scale = lerp(current_scale, 0.0, 0.04)
-		ScaleDirection.UP:
-			if current_scale < MAX_SCALE:
-				current_scale = lerp(current_scale, 2.0, 0.04)
+signal card_selected(card: Card)
 
+func _ready():
+	match card_type:
+		CardType.Attack:
+			particles.modulate = Color.from_string("#c52956", Color.RED)
+		CardType.Defense:
+			particles.modulate = Color.from_string("#4728f6", Color.BLUE)
+		CardType.Ability:
+			particles.modulate = Color.from_string("#198641", Color.SEA_GREEN)
+			
+func _process(_delta):
+	if hovered:
+		current_scale = lerp(MAX_SCALE, 2.0, 0.04)
+	else: 
+		current_scale = lerp(MIN_SCALE, 0.0, 0.04)
+
+	if selected:
+		particles.emitting = true
+		modulate = Color(1, 1, 1, 1)
+	else:
+		particles.emitting = false
+		modulate = Color(0.86, 0.86, 0.86, 1)
+		
 	set_scale(Vector2(current_scale, current_scale))
 
 func _on_area_2d_mouse_entered():
-	scale_direction = ScaleDirection.UP
+	hovered = true
 
 func _on_area_2d_mouse_exited():
-	scale_direction = ScaleDirection.DOWN
+	hovered = false
+
+func _on_area_2d_input_event(_viewport, event, _shape_idx):
+	if event.is_action_pressed("cardboard_click"):
+		selected  = !selected
+		if selected:
+			emit_signal("card_selected", self)
+
--- cardboard/cursor.gd
+++ cardboard/cursor.gd
@@ -2,8 +2,84 @@ extends Node
 
 var cursor = load("res://cardboard/assets/mouse.png")
 
-# TODO
+const card: PackedScene = preload("res://cardboard/card.tscn")
+
+var attack_player_card: Card
+var defense_player_card: Card
+var ability_player_card: Card
+var attack_boss_card: Card
+var defense_boss_card: Card
+var ability_boss_card: Card
+
+var defense_color = ""
+
+var cards = []
+
+@onready var exchange_button: Sprite2D = $ExchangeButton
+
+func _process(delta):
+	selection_pair(attack_player_card, attack_boss_card)
+	selection_pair(defense_player_card, defense_boss_card)
+	selection_pair(ability_player_card, ability_boss_card)
+
 func _ready():
+	attack_player_card = card.instantiate()
+	attack_player_card.card_type = Card.CardType.Attack
+	
+	defense_player_card = card.instantiate()
+	defense_player_card.card_type = Card.CardType.Defense
+		
+	ability_player_card = card.instantiate()
+	ability_player_card.card_type = Card.CardType.Ability
+	
+	$PlayerHand.add_child(attack_player_card)
+	$PlayerHand.add_child(defense_player_card)
+	$PlayerHand.add_child(ability_player_card)
+	
+	attack_player_card.position = $PlayerHand/CardSlot1.position
+	defense_player_card.position = $PlayerHand/CardSlot2.position
+	ability_player_card.position = $PlayerHand/CardSlot3.position
+	
+	attack_boss_card = card.instantiate()
+	attack_boss_card.card_type = Card.CardType.Attack
+	
+	defense_boss_card = card.instantiate()
+	defense_boss_card.card_type = Card.CardType.Defense
+
+	ability_boss_card = card.instantiate()
+	ability_boss_card.card_type = Card.CardType.Ability
+		
+	$BossHand.add_child(attack_boss_card)
+	$BossHand.add_child(defense_boss_card)
+	$BossHand.add_child(ability_boss_card)
+	
+	attack_boss_card.position = $BossHand/CardSlot1.position
+	defense_boss_card.position = $BossHand/CardSlot2.position
+	ability_boss_card.position = $BossHand/CardSlot3.position
+	
+	cards.append(attack_player_card)
+	cards.append(defense_player_card)
+	cards.append(ability_player_card)
+	cards.append(attack_boss_card)
+	cards.append(defense_boss_card)
+	cards.append(ability_boss_card)
+	
+	for card_: Card in cards:
+		card_.card_selected.connect(_on_card_selected)
+		
 	Input.set_custom_mouse_cursor(cursor, Input.CURSOR_ARROW, Vector2(0, 0))
 	Input.set_custom_mouse_cursor(cursor, Input.CURSOR_IBEAM, Vector2(0, 0))
 
+
+func _on_card_selected(selected_card: Card):
+	$ExchangeButton/CPUParticles2D.emitting = true
+	$ExchangeButton/CPUParticles2D.modulate = selected_card.particles.modulate
+	
+	for card: Card in cards:
+		card.selected = false
+	selected_card.selected = true
+	
+func selection_pair(card: Card, pair: Card): 
+	if card.selected or pair.selected:
+		card.selected = true
+		pair.selected = true
